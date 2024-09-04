dba40209ac77db1ec9634eff60f2c5a1f2bd6f37
İrem Nur Şener <irem.sener19@gmail.com>
2024-09-04 12:45:24 +0300

    Upload Image functionality added.

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -12,9 +12,12 @@ namespace BulkyWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment= webHostEnvironment;
         }
 
 
@@ -27,6 +30,8 @@ namespace BulkyWeb.Areas.Admin.Controllers
            
             return View(objCategoryList);
         }
+
+
 
         public IActionResult Upsert(int?id)
         {
@@ -67,6 +72,25 @@ namespace BulkyWeb.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+
+                if (file!=null)
+                {
+                    string filename =Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, filename),FileMode.Create)) { 
+
+                        file.CopyTo(fileStream);
+
+
+                    }
+
+                    productVM.Product.ImageUrl = @"\images\product\" + filename;
+
+
+                }
+
                 _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
